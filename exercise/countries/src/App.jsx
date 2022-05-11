acd3d6d31c24e70881722292045396633e6fb835
Ricky0625 <rickywong0625@gmail.com>
2022-05-11 19:52:17 +0800

    regular update

--- exercise/countries/src/App.jsx
+++ exercise/countries/src/App.jsx
@@ -1,38 +1,68 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Search from './components/Search'
+import Search from "./components/Search";
 
-const List = ({list}) => {
-  let index = 0;
-  return (
-    <div>
-      {list.map(country => <p key={country.cca2}>{country.name.common}</p>)}
-    </div>
-  )
-}
+const List = ({ list, tooMany }) => {
+  // console.log(tooMany)
+  if (tooMany) {
+    return <p>Too many matches, specify another filter</p>;
+  } else {
+    return (
+      <div>
+        {list.map((country) => (
+          <p key={country.cca2}>{country.name.common}</p>
+        ))}
+      </div>
+    );
+  }
+};
 
 const App = () => {
   // useState
   const [countryList, setCountryList] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [filteredList, setFilteredList] = useState(countryList);
+  const [tooMany, setTooMany] = useState(false);
 
   // useEffect
   const hook = () => {
     axios.get(`https://restcountries.com/v3.1/all`).then((response) => {
       setCountryList(response.data);
+      setFilteredList(response.data);
     });
   };
   useEffect(hook, []);
   // console.log(countryList)
-  // functions
+
+  // user-defined functions
+  const isTooMany = () => {
+    if (filteredList.length > 0 && filteredList.length <= 10) {
+      setTooMany(false);
+    } else {
+      setTooMany(true);
+    }
+  };
+
   const filterThis = (e) => {
-    setSearchTerm(e.target.value);
+    const searchKey = e.target.value;
+    // to update the input field
+    setSearchTerm(searchKey);
+    if (searchKey.length === 0) {
+      setTooMany(false);
+    }
+    const newList = countryList.filter((country) => {
+      const name = country.name.common.toLowerCase();
+      return name.includes(searchKey.toLowerCase());
+    });
+    setFilteredList(newList);
+    isTooMany();
+    console.log(filteredList.length);
   };
 
   return (
     <div>
       <Search value={searchTerm} handleOnChange={filterThis} />
-      <List list={countryList}/>
+      <List list={filteredList} tooMany={tooMany} />
     </div>
   );
 };
